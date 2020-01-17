185c928fe4bf68bba6b756d25b5b88666dd14a86
mathewsd3 <mathewsdawn3@gmail.com>
2020-01-17 12:07:53 -0500

    first commit

---
+++ exercise-first-page.php
@@ -0,0 +1,23 @@
+<?php
+$name = 'abc def';
+$age = 25;
+$city = 'Montreal';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+<title>Exercise - First Page</title>
+</head>
+
+<body>
+<h1>Presentation</h1>
+
+<p>
+Hello my name is <b><?php echo $name ?></b> and I am <b><?php echo $age ?></b> years old. <br>
+I live in the city of <b><?php echo $city ?></b>.
+</p>
+</body>
+
+</html>
